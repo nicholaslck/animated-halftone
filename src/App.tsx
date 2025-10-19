bf9802bb1b6f087cbc4e4502c6001a3ed59041ff
Nicholas Lau <nicholaslck@gmail.com>
2025-10-20 09:40:36 +1300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {/*Canvas*/}
+      <div></div>
+
+      {/*Control*/}
+      <div></div>
+    </>
+  );
+}
+
+export default App;
